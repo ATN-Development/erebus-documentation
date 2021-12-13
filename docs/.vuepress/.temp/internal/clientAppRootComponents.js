49d6a8c8ae1a9e9ef1fa8781c7c0fe21e4859fb3
NotReallyEight <wangzndg@gmail.com>
2021-12-13 21:19:11 +0100

    Bruh

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/media/notreallyeight/Eight's Things Lol/erebus-docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="hello-world" tabindex="-1"><a class="header-anchor" href="#hello-world" aria-hidden="true">#</a> Hello world.</h1>
-</template>
