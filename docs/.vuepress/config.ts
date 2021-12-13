a516bc2ffdd59b34b77d6cf20602dc52a496f2c7
NotReallyEight <wangzndg@gmail.com>
2021-12-13 21:38:37 +0100

    Edit config to make the site overall look better

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,9 +2,10 @@ import { defineUserConfig } from "vuepress";
 import type { DefaultThemeOptions } from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: "en-UK",
-  title: "Hello world",
-  description: "Test description",
+  base: "/",
+  lang: "en-GB",
+  title: "Erebus",
+  description: "Erebus official documentation.",
   themeConfig: {
     logo: "https://cdn.discordapp.com/icons/746291190009430049/a6895db35c8c486cdf42fd65c0de20b9.png?size=4096",
   },
