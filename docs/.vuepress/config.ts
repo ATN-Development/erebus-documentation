7d4c992a73ddefd1857a1b3f820d8684bd3cce50
NotReallyEight <wangzndg@gmail.com>
2021-12-30 18:06:49 +0100

    Maybe?

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,6 +17,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     ],
   ],
   themeConfig: {
+    activeHeaderLinks: false,
     logo: "https://cdn.discordapp.com/icons/746291190009430049/a6895db35c8c486cdf42fd65c0de20b9.png?size=4096",
     smoothScroll: true,
     sidebar: {
